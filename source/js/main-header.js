bedecd91b47f59de2762c20b9d58ebae64c73cdc
Vitaliy Vasiliev <93582413+Ierixorn@users.noreply.github.com>
2022-05-11 17:38:55 +0300

    Главная, работа над планшетной версией

---
+++ source/js/main-header.js
@@ -0,0 +1,18 @@
+let mainHead = document.querySelector('.main-header');
+let navMain = document.querySelector('.main-navigation');
+let navToggle = document.querySelector('.main-navigation__toggle');
+let mainIndex = document.querySelector('.main-index');
+
+mainIndex.classList.remove('main-index--nojs');
+mainHead.classList.remove('main-header--nojs');
+navMain.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
